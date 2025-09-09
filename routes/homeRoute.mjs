ce10347a5c1f47455f04ea0a2da0329844745ab5
Melkamu Woldemariam <melkamu2@yahoo.com>
2025-09-08 21:27:57 -0400

    put, post and delete methods added on userData

--- routes/homeRoute.mjs
+++ routes/homeRoute.mjs
@@ -49,7 +49,7 @@ router
 
 router
 .route("/userData/:id")
-.get ((req, res)=>{
+.get ((req, res) => {
     const id = req.params.id;
     const data = userData.find((data)=> id == data.id)
     if (data){
@@ -58,5 +58,62 @@ router
             res.status(400).send("No user Id found!")}   
 })
 
+.post((req, res) => {
+    const body = req.body;
+    const newUser = {
+        id: userData.length + 1,
+        fullName:body.fullName,
+        title:body.title,
+        summary:body.summary,
+        email:body.email,
+        phone:body.phone,
+        photoUrl:body.photoUrl,
+        skills:body.skills
+    }
+    userData.push(newUser);
+    res.json(newUser);
+})
+
+.put ((req, res) => {
+    const id = req.params.id;
+    const body = req.body;
+    // find the index of the user with the matching ID
+    const userIndex = userData.findIndex(user => user.id == id);
+
+    // If user is not found, return error
+    if (userIndex === -1) {
+        return res.status(404).json({message: "User not found"})
+    }
+
+    // Update the record
+    userData[userIndex] = {
+            id: id,
+            fullName:body.fullName,
+            title:body.title,
+            summary:body.summary,
+            email:body.email,
+            phone:body.phone,
+            photoUrl:body.photoUrl,
+            skills:body.skills
+    }
+
+    // Return updated data
+    res.json(userData[userIndex]);
+
+})
+
+.delete((req, res) => {
+    const id = req.params.id;
+    // find the index of the user with the matching ID
+    const userIndex = userData.findIndex(user => user.id == id);
+
+    // If user is not found, return error
+    if (userIndex === -1) {
+        return res.status(404).json({message: "User not found"})
+    }
+    // Remove the user at the index
+    userData.splice(userIndex, 1);
+    res.json({message: "User Deleted successfully.", remainingUsers: userData});
+})
 export default router;
 
